a6062f8421bd66166d3d550e673f59a082b41d06
XRZ8X <99104130+XRZ8X@users.noreply.github.com>
2022-02-06 05:55:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -139,5 +139,5 @@ def redirect_message():
 
 if __name__ == "__main__":
     bot.remove_webhook()
-    bot.set_webhook(url="https://mtgar.herokuapp.com/"+str(BOT_TOKEN))
+    bot.set_webhook(url="https://m.herokuapp.com/"+str(BOT_TOKEN))
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
